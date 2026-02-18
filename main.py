7b3ed47b2102087d95b92a9ffd0e32683cd8e100
Barath-Dharmaraj <barathdp@gmail.com>
2026-02-18 21:16:22 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+a=10
+b=5
+def add(x,y):
+    print("Sum:",a+b)
+def sub(x,y):
+    print("Diff:",a-b)
+def mul(x,y):
+    print("Mul:",a*b)
+def div(x,y):
+    print("Div:",a/b)
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+
+
